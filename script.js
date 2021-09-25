c32f7a518a0a78f40bce6d7194f41e2f795b5e22
Angela Guarin <gurive.3@gmail.com>
2021-09-25 12:19:50 -0500

    Se suben archivos de Tarea 1 - Conceptos Básicos

---
+++ script.js
@@ -0,0 +1,16 @@
+let num1 = 5;
+let num2 = 7;
+let suma = 0;
+let resta = 0;
+let multiplicacion = 0;
+let division = 0;
+
+suma=num1+ num2;
+resta=num1-num2;
+multiplicacion=num1*num2;
+division=num1/num2;
+
+console.log(`La suma de los números es: ${suma}`);
+console.log(`La resta de los números es: ${resta}`);
+console.log(`La multiplicación de los números es: ${multiplicacion}`);
+console.log(`La división de los números es: ${division}`);
